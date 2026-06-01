683e735a60d8b06306567db621b03a3d2767fb82
Shyaminda Senevirathna <shyamindasenevirathna@gmail.com>
2026-06-01 12:38:28 +0530

    add backend configuration for S3 with state management settings

--- tf-s3-provision/main.tf
+++ tf-s3-provision/main.tf
@@ -1,4 +1,11 @@
 terraform {
+	backend "s3" {
+		bucket = "tf-s3-provision-state-demo-bucket"
+		key    = "dev/terraform.tfstate"
+		region = "us-east-1"
+		encrypt = true
+		use_lockfile = true
+	}
 	required_providers {
 		aws = {
 			source  = "hashicorp/aws"
