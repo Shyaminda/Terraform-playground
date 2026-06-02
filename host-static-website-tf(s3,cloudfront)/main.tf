0643cf733edfb61ae1e9fdee0b8990d17d1971e1
Shyaminda Senevirathna <shyamindasenevirathna@gmail.com>
2026-06-02 21:54:59 +0530

    feat: add S3 bucket policy to allow CloudFront access

--- host-static-website-tf(s3,cloudfront)/main.tf
+++ host-static-website-tf(s3,cloudfront)/main.tf
@@ -1,14 +1,14 @@
 resource "aws_s3_bucket" "static_web_bucket" {
-	bucket = var.bucket_name
+  bucket = var.bucket_name
 }
 
 resource "aws_s3_public_access_block" "public_access_block" {
-	bucket = aws_s3_bucket.static_web_bucket.id
+  bucket = aws_s3_bucket.static_web_bucket.id
 
-	block_public_acls       = true
-	block_public_policy     = true
-	ignore_public_acls      = true
-	restrict_public_buckets = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_cloudfront_origin_access_control" "oac" {
@@ -17,4 +17,34 @@ resource "aws_cloudfront_origin_access_control" "oac" {
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
+}
+
+resource "aws_s3_bucket_policy" "allow_cf_access" {
+  bucket = aws_s3_bucket.static_web_bucket.id
+  depends_on = [aws_public_access_block.public_access_block]
+
+  policy = jsonencode( //generated from https://awspolicygen.s3.amazonaws.com/policygen.html
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "AllowCloudFront",
+          "Effect" : "Allow",
+          "Principal" : {
+            "AWS" : "cloudfront.amazonaws.com"
+          },
+          "Action" : [
+            "s3:GetObject",
+            "s3:ListBucket"
+          ],
+          "Resource" : "${aws_s3_bucket.static_web_bucket.arn}/*",
+          "Condition" : {
+            "StringEquals" : {
+              "aws:SourceArn" : "aws_cloudfront_distribution.s3_distribution.arn"
+            }
+          }
+        }
+      ]
+    }
+  )
 }
