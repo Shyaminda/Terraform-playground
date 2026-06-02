6975f9fd3730f012c55902d8097594ccae922873
Shyaminda Senevirathna <shyamindasenevirathna@gmail.com>
2026-06-02 21:27:11 +0530

    feat: add S3 public access block configuration with tf

--- host-static-website-tf(s3,cloudfront)/main.tf
+++ host-static-website-tf(s3,cloudfront)/main.tf
@@ -1,3 +1,12 @@
 resource "aws_s3_bucket" "static_web_bucket" {
 	bucket = var.bucket_name
 }
+
+resource "aws_s3_public_access_block" "public_access_block" {
+	bucket = aws_s3_bucket.static_web_bucket.id
+
+	block_public_acls       = true
+	block_public_policy     = true
+	ignore_public_acls      = true
+	restrict_public_buckets = true
+}
