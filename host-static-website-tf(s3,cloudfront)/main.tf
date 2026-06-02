19d93e2ead7eedd201fd0cb1da40c5381447af19
Shyaminda Senevirathna <shyamindasenevirathna@gmail.com>
2026-06-02 23:13:13 +0530

    feat: update S3 bucket policy and add CloudFront distribution configuration

--- host-static-website-tf(s3,cloudfront)/main.tf
+++ host-static-website-tf(s3,cloudfront)/main.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "static_web_bucket" {
   bucket = var.bucket_name
 }
 
-resource "aws_s3_public_access_block" "public_access_block" {
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
   bucket = aws_s3_bucket.static_web_bucket.id
 
   block_public_acls       = true
@@ -20,8 +20,8 @@ resource "aws_cloudfront_origin_access_control" "oac" {
 }
 
 resource "aws_s3_bucket_policy" "allow_cf_access" {
-  bucket = aws_s3_bucket.static_web_bucket.id
-  depends_on = [aws_public_access_block.public_access_block]
+  bucket     = aws_s3_bucket.static_web_bucket.id
+  depends_on = [aws_s3_bucket_public_access_block.public_access_block]
 
   policy = jsonencode( //generated from https://awspolicygen.s3.amazonaws.com/policygen.html
     {
@@ -31,20 +31,89 @@ resource "aws_s3_bucket_policy" "allow_cf_access" {
           "Sid" : "AllowCloudFront",
           "Effect" : "Allow",
           "Principal" : {
-            "AWS" : "cloudfront.amazonaws.com"
+            "Service" : "cloudfront.amazonaws.com"
           },
           "Action" : [
             "s3:GetObject",
-            "s3:ListBucket"
           ],
-          "Resource" : "${aws_s3_bucket.static_web_bucket.arn}/*",
+          "Resource" : "${aws_s3_bucket.static_web_bucket.arn}/*",  //allow access to all objects in the bucket
           "Condition" : {
             "StringEquals" : {
-              "aws:SourceArn" : "aws_cloudfront_distribution.s3_distribution.arn"
+              "aws:SourceArn" : aws_cloudfront_distribution.s3_distribution.arn
             }
           }
         }
       ]
     }
   )
+}
+
+resource "aws_s3_object" "objects" {
+  for_each = fileset("${path.module}/static-web-app", "**/*")
+  bucket   = aws_s3_bucket.static_web_bucket.id
+  key    = each.value
+  source = "${path.module}/static-web-app/${each.value}"
+
+  etag = filemd5("${path.module}/static-web-app/${each.value}")
+  content_type = lookup(
+    {
+      "html" : "text/html",
+      "css" : "text/css",
+      "js" : "application/javascript",
+      "json" : "application/json",
+      "png" : "image/png",
+      "jpg" : "image/jpeg",
+      "jpeg" : "image/jpeg",
+      "gif" : "image/gif",
+      "svg" : "image/svg+xml",
+      "ico" : "image/x-icon",
+      "txt" : "text/plain",
+    },
+    split(".", each.value)[length(split(".", each.value)) - 1],
+    "application/octet-stream"
+  )
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.static_web_bucket.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+    origin_id                = local.origin_id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Some demo comment"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
