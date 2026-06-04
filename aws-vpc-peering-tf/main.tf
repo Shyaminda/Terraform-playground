eabee949609523f85877f4b25473aa9e8dba473c
Shyaminda Senevirathna <shyamindasenevirathna@gmail.com>
2026-06-04 13:10:10 +0530

    feat: add primary and secondary VPCs with public subnets
    - Created primary VPC with DNS support and hostnames enabled.
    - Created secondary VPC with DNS support and hostnames enabled.
    - Added public subnet in primary VPC with public IP mapping on launch.
    - Added public subnet in secondary VPC with public IP mapping on launch.
    - Configured tags for environment and region identification.

---
+++ aws-vpc-peering-tf/main.tf
@@ -0,0 +1,47 @@
+resource "aws_vpc" "primary_vpc" {
+  cidr_block           = var.primary_vpc_cidr
+  provider             = aws.primary
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "primary-vpc-${var.primary_region}"
+  }
+}
+
+resource "aws_vpc" "secondary_vpc" {
+  cidr_block           = var.secondary_vpc_cidr
+  provider             = aws.secondary
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "secondary-vpc-${var.secondary_region}"
+  }
+}
+
+resource "aws_subnet" "primary_subnet" {
+  provider                = aws.primary
+  vpc_id                  = aws_vpc.primary_vpc.id
+  cidr_block              = var.primary_vpc_cidr
+  availability_zone       = data.aws_availability_zones.primary.names[0]
+  map_public_ip_on_launch = true //making instances public
+
+  tags = {
+    Name        = "primary-Subnet-${var.primary_region}"
+    Environment = "dev"
+  }
+}
+
+resource "aws_subnet" "secondary_subnet" {
+  provider                = aws.secondary
+  vpc_id                  = aws_vpc.secondary_vpc.id
+  cidr_block              = var.secondary_vpc_cidr
+  availability_zone       = data.aws_availability_zones.secondary.names[0]
+  map_public_ip_on_launch = true //making instances public
+
+  tags = {
+    Name        = "primary-Subnet-${var.secondary_region}"
+    Environment = "dev"
+  }
+}
