298fa78a35922ea63ff83fb2fbead4fa8dd391b0
Shyaminda Senevirathna <shyamindasenevirathna@gmail.com>
2026-06-04 14:24:28 +0530

    feat: format variable definitions for consistency in variables.tf

--- aws-vpc-peering-tf/variables.tf
+++ aws-vpc-peering-tf/variables.tf
@@ -1,17 +1,33 @@
 variable "primary_region" {
-	type = string
-	default = "us-east-1"
+  type    = string
+  default = "us-east-1"
 }
 
 variable "secondary_region" {
-	type = string
-	default = "us-west-2"
+  type    = string
+  default = "us-west-2"
 }
 
 variable "primary_vpc_cidr" {
-	default = "10.0.0.0/16"
+  default = "10.0.0.0/16"
 }
 
 variable "secondary_vpc_cidr" {
-	default = "10.1.0.0/16"
+  default = "10.1.0.0/16"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "primary_key_name" {
+  description = "Name of the SSH key pair for Primary VPC instance (us-east-1)"
+  type        = string
+  default     = ""
+}
+
+variable "secondary_key_name" {
+  description = "Name of the SSH key pair for Secondary VPC instance (us-west-2)"
+  type        = string
+  default     = ""
 }
