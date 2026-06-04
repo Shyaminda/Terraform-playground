81e216c38be6f43f342968fc244e5b3796d34f3d
Shyaminda Senevirathna <shyamindasenevirathna@gmail.com>
2026-06-04 13:31:04 +0530

    feat: add VPC peering connections and update route tables and internet gateways

--- aws-vpc-peering-tf/main.tf
+++ aws-vpc-peering-tf/main.tf
@@ -47,63 +47,89 @@ resource "aws_subnet" "secondary_subnet" {
 }
 
 resource "aws_internet_gateway" "primary_igw" {
-	provider = aws.primary
-	vpc_id = aws_vpc.primary_vpc.id
+  provider = aws.primary
+  vpc_id   = aws_vpc.primary_vpc.id
 
-	tags = {
-		Name = "primary-igw-${var.primary_region}"
-		Environment = "dev"
-	}
+  tags = {
+    Name        = "primary-igw-${var.primary_region}"
+    Environment = "dev"
+  }
 }
 
 resource "aws_internet_gateway" "secondary_igw" {
-	provider = aws.secondary
-	vpc_id = aws_vpc.secondary_vpc.id
+  provider = aws.secondary
+  vpc_id   = aws_vpc.secondary_vpc.id
 
-	tags = {
-		Name = "secondary-igw-${var.secondary_region}"
-		Environment = "dev"
-	}
+  tags = {
+    Name        = "secondary-igw-${var.secondary_region}"
+    Environment = "dev"
+  }
 }
 
 resource "aws_route_table" "primary_rt" {
-	provider = aws.primary
-	vpc_id = aws_vpc.primary_vpc.id
+  provider = aws.primary
+  vpc_id   = aws_vpc.primary_vpc.id
 
-	route {
-		cidr_block = "0.0.0.0/0"
-		gateway_id = aws_internet_gateway.primary_igw.id
-	}
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.primary_igw.id
+  }
 
-	tags = {
-		Name = "primary-rt"
-		Environment = "dev"
-	}
+  tags = {
+    Name        = "primary-rt"
+    Environment = "dev"
+  }
 }
 
 resource "aws_route_table" "secondary_rt" {
-	provider = aws.secondary
-	vpc_id = aws_vpc.secondary_vpc.id
+  provider = aws.secondary
+  vpc_id   = aws_vpc.secondary_vpc.id
 
-	route {
-		cidr_block = "0.0.0.0/0"
-		gateway_id = aws_internet_gateway.secondary_igw.id
-	}
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.secondary_igw.id
+  }
 
-	tags = {
-		Name = "secondary-rt"
-		Environment = "dev"
-	}
+  tags = {
+    Name        = "secondary-rt"
+    Environment = "dev"
+  }
 }
 
 resource "aws_route_table_association" "primary_rta" {
-	provider = aws.primary
-	subnet_id = aws_subnet.primary_subnet.id
-	route_table_id = aws_route_table.primary_rt.id
+  provider       = aws.primary
+  subnet_id      = aws_subnet.primary_subnet.id
+  route_table_id = aws_route_table.primary_rt.id
 }
 
 resource "aws_route_table_association" "secondary_rta" {
-	provider = aws.secondary
-	subnet_id = aws_subnet.secondary_subnet.id
-	route_table_id = aws_route_table.secondary_rt.id
+  provider       = aws.secondary
+  subnet_id      = aws_subnet.secondary_subnet.id
+  route_table_id = aws_route_table.secondary_rt.id
+}
+
+resource "aws_vpc_peering_connection" "primary_to_secondary" {
+  provider    = aws.primary
+  vpc_id      = aws_vpc.primary_vpc.id
+  peer_vpc_id = aws_vpc.secondary_vpc.id
+  peer_region = var.secondary_region
+  auto_accept = false
+
+  tags = {
+    Name        = "primary-to-secondary-peering"
+    Environment = "dev"
+  }
+}
+
+resource "aws_vpc_peering_connection" "secondary_to_primary" {
+  provider    = aws.secondary
+  vpc_id      = aws_vpc.secondary_vpc.id
+  peer_vpc_id = aws_vpc.primary_vpc.id
+  peer_region = var.primary_region
+  auto_accept = false
+
+  tags = {
+    Name        = "secondary-to-primary-peering"
+    Environment = "dev"
+  }
 }
